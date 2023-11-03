2eb27929c182b0920bb5ee07d5517b26ce9fd205
Roman Paholík <wizzardsk@gmail.com>
2023-11-03 18:07:14 +0100

    tried sharper images

--- Named_Titles/resize.sh
+++ Named_Titles/resize.sh
@@ -1,7 +1,7 @@
 find . -type f -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -exec sh -c 'identify -format "%w %i\n" "$0" | {
   read width file
-  if [ "$width" -gt 320 ]; then
-    convert "$file" -resize 320x "$file"
+  if [ "$width" -lt 320 ]; then
+    convert "$file" -resize 320x243! -filter point "$file"
     echo "Zmenšený súbor: $file"
   else
     echo "Preskočený súbor: $file"
